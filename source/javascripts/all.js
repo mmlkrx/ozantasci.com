5e4f79f45872188aa4ef9396b7887fd2b2e04470
Matthias Loker <mmloker@gmail.com>
2015-05-25 21:34:37 +0200

    Add vanilla js class highlighting for active link

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
 //= require_tree .
+
+document.addEventListener('click', function(e){
+  e = e || window.event;
+  if (e.target.parentElement.parentElement.parentElement.className === "nav") {
+    list = e.target.parentElement.parentElement.children
+
+    for (i in list) {
+      if (i < 7) {
+        list[i].firstChild.className = '';
+      }
+    }
+    e.target.className = 'active';
+  }
+});
