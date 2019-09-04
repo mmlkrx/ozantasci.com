aeda73a3010943a8a8f3640d4ffe55201b7fb7a8
Matthias Michael Loker <m@theloker.com>
2019-09-04 23:00:00 +0200

    Add rest of available videos, go wild with layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+    	inset: {
+    		'1/2': '50%',
+    		'-10': '-2.5rem',
+    		'-12': '-3rem',
+    		'-16': '-4rem',
+    		'-20': '-5rem',
+    		'-24': '-6rem',
+    		'-32': '-8rem',
+    	}
+    }
   },
   variants: {},
   plugins: []
