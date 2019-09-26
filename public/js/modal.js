9d6eb43e49e6a4ff687e6cd33845323aff4a2a13
Matthias Michael Loker <m@theloker.com>
2019-09-26 14:06:23 +0200

    Add close button to video modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,7 +2,7 @@ var modal = document.getElementById("myModal");
 var aboutPageContent = document.getElementById("about-content");
 
 document.addEventListener('click', function (event) {
-  if (!(event.target.matches('#about-btn') || event.target.closest('.video') || event.target.closest('#close-btn'))) return; // if we don't click any of these special elements, don't do anything, if we do, continue with doing different things depending on the element
+  if (!(event.target.matches('#about-btn') || event.target.closest('.video') || event.target.closest('.close-btn'))) return; // if we don't click any of these special elements, don't do anything, if we do, continue with doing different things depending on the element
   if (event.target.matches('#about-btn')) {
     aboutPageContent.style.display = "flex";
     aboutPageContent.className = 'modal w-screen h-screen flex justify-center items-center text-white absolute top-0';
@@ -20,7 +20,7 @@ document.addEventListener('click', function (event) {
       modal.firstElementChild.innerHTML = newHTML; // copy the HTML to the modal
       modal.style.display = "flex"; // show the modal
     }
-  } else if (event.target.closest('#close-btn')) {
+  } else if (event.target.closest('.close-btn')) {
     modal.style.display = "none";
     aboutPageContent.style.display = "none";
   }
