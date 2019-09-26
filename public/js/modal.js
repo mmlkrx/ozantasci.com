2f9b9cdcdb4f18a32e6068da583f8a180efa4f58
Matthias Michael Loker <m@theloker.com>
2019-09-26 11:51:29 +0200

    Fix close button not closing on click on svg path
    Using event.target.closest('#close-btn') works. Also decided to have one global event listener
    see https://gomakethings.com/detecting-click-events-on-svgs-with-vanilla-js-event-delegation/

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,8 +1,30 @@
 var modal = document.getElementById("myModal");
-var videoContainers = document.getElementsByClassName("video");
 var aboutPageContent = document.getElementById("about-content");
-var aboutButton = document.getElementById("about-btn");
-var closeButton = document.getElementById("close-btn");
+
+document.addEventListener('click', function (event) {
+  if (!(event.target.matches('#about-btn') || event.target.closest('.video') || event.target.closest('#close-btn'))) return; // if we don't click any of these special elements, don't do anything, if we do, continue with doing different things depending on the element
+  if (event.target.matches('#about-btn')) {
+    aboutPageContent.style.display = "flex";
+    aboutPageContent.className = 'modal w-screen h-screen flex justify-center items-center text-white absolute top-0';
+  } else if (event.target.closest('.video')) {
+    if (event.target.parentElement.firstElementChild.classList.contains('video-portrait')) {
+      event.target.parentElement.firstElementChild.className = "block video-portrait"
+      var newHTML = event.target.parentElement.firstElementChild.outerHTML; // save it's HTML
+      event.target.parentElement.firstElementChild.className = "hidden video-portrait"; // hiding so it's not showing up behind the modal
+      modal.firstElementChild.innerHTML = newHTML; // copy the HTML to the modal
+      modal.style.display = "flex"; // show the modal
+    } else {
+      event.target.parentElement.firstElementChild.className = "block"; // make hidden video iframe appear
+      var newHTML = event.target.parentElement.firstElementChild.outerHTML; // save it's HTML
+      event.target.parentElement.firstElementChild.className = "hidden"; // hiding so it's not showing up behind the modal
+      modal.firstElementChild.innerHTML = newHTML; // copy the HTML to the modal
+      modal.style.display = "flex"; // show the modal
+    }
+  } else if (event.target.closest('#close-btn')) {
+    modal.style.display = "none";
+    aboutPageContent.style.display = "none";
+  }
+}, false);
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -12,35 +34,4 @@ window.onclick = function(event) {
   if (event.target == aboutPageContent) {
     aboutPageContent.style.display = "none";
   }
-  if (event.target == closeButton) {
-    modal.style.display = "none";
-    aboutPageContent.style.display = "none";
-  }
 }
-
-var myFunction = function() {
-  if (this.firstElementChild.classList.contains("video-portrait")) {
-    this.firstElementChild.className = "block video-portrait"
-    var newHTML = this.firstElementChild.outerHTML; // save it's HTML
-    this.firstElementChild.className = "hidden video-portrait"; // hiding so it's not showing up behind the modal
-    modal.firstElementChild.innerHTML = newHTML; // copy the HTML to the modal
-    modal.style.display = "flex"; // show the modal
-  } else {
-    this.firstElementChild.className = "block"; // make hidden video iframe appear
-    var newHTML = this.firstElementChild.outerHTML; // save it's HTML
-    this.firstElementChild.className = "hidden"; // hiding so it's not showing up behind the modal
-    modal.firstElementChild.innerHTML = newHTML; // copy the HTML to the modal
-    modal.style.display = "flex"; // show the modal
-  }
-};
-
-var aboutPage = function() {
-  aboutPageContent.style.display = "flex";
-  aboutPageContent.className = 'modal w-screen h-screen flex justify-center items-center text-white absolute top-0';
-};
-
-for (var i = 0; i < videoContainers.length; i++) {
-  videoContainers[i].addEventListener('click', myFunction, false);
-}
-
-aboutButton.addEventListener('click', aboutPage, false);
