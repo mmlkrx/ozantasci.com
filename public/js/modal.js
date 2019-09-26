abc1912cc9c9d5315b6f0f1141827f8abfcf4cdd
Matthias Michael Loker <m@theloker.com>
2019-09-26 20:12:29 +0200

    Dim videos when selecting editing or grading

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,7 +2,7 @@ var modal = document.getElementById("myModal");
 var aboutPageContent = document.getElementById("about-content");
 
 document.addEventListener('click', function (event) {
-  if (!(event.target.matches('#about-btn') || event.target.closest('.video') || event.target.closest('.close-btn'))) return; // if we don't click any of these special elements, don't do anything, if we do, continue with doing different things depending on the element
+  if (!(event.target.matches('#about-btn') || event.target.closest('.video') || event.target.closest('.close-btn') || event.target.closest('#editing-btn') || event.target.closest('#ozzy-btn') || event.target.closest('#grading-btn'))) return; // if we don't click any of these special elements, don't do anything, if we do, continue with doing different things depending on the element
   if (event.target.matches('#about-btn')) {
     aboutPageContent.style.display = "flex";
     aboutPageContent.className = 'modal w-screen h-screen flex justify-center items-center text-white absolute top-0';
@@ -23,6 +23,37 @@ document.addEventListener('click', function (event) {
   } else if (event.target.closest('.close-btn')) {
     modal.style.display = "none";
     aboutPageContent.style.display = "none";
+  } else if (event.target.closest('#editing-btn')) {
+    // reset editing videos back to 100% brightness
+    let editingVideos = document.getElementsByClassName('video-editing');
+    for(var i=0; i < editingVideos.length; i++) {
+      editingVideos[i].style.filter = "brightness(100%)";
+    }
+    // dim grading videos
+    let gradingVideos = document.getElementsByClassName('video-grading');
+    for(var i=0; i < gradingVideos.length; i++) {
+      if (!gradingVideos[i].classList.contains('video-editing')) {
+        gradingVideos[i].style.filter = "brightness(15%)";
+      }
+    }
+  } else if (event.target.closest('#ozzy-btn')) {
+    let allVideos = document.getElementsByClassName('video');
+    for(var i=0; i < allVideos.length; i++) {
+      allVideos[i].style.filter = "brightness(100%)";
+    }
+  } else if (event.target.closest('#grading-btn')) {
+    // reset grading videos back to 100% brightness
+    let gradingVideos = document.getElementsByClassName('video-grading');
+    for(var i=0; i < gradingVideos.length; i++) {
+      gradingVideos[i].style.filter = "brightness(100%)";
+    }
+    // dim editing videos
+    let editingVideos = document.getElementsByClassName('video-editing');
+    for(var i=0; i < editingVideos.length; i++) {
+      if (!editingVideos[i].classList.contains('video-grading')) {
+        editingVideos[i].style.filter = "brightness(15%)";
+      }
+    }
   }
 }, false);
 
