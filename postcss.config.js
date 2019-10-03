669ba1b3867b7d33b3ba72b8627aa8ac41554b2e
Matthias Michael Loker <m@theloker.com>
2019-10-03 21:11:39 +0200

    Enable purgecss for smaller css filesize

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './public/index.html',
+  ],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
