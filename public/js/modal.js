80f04e31c0a27549d30e4d3ef95d5e99737aafc8
Matthias Michael Loker <m@theloker.com>
2019-10-03 17:01:14 +0200

    Refactor event handling

--- public/js/modal.js
+++ public/js/modal.js
@@ -4,16 +4,43 @@ var imprintContent = document.getElementById("imprint-content");
 var websiteGate = document.getElementById("website-gate");
 var videoName = document.getElementById("video-name");
 
+var allVideos = document.getElementsByClassName('video');
+var editingVideos = document.getElementsByClassName('video-editing');
+var gradingVideos = document.getElementsByClassName('video-grading');
+
+var editingButton = document.getElementById("editing-btn");
+var gradingButton = document.getElementById("grading-btn");
+var ozzyButton = document.getElementById("ozzy-btn");
+var aboutButton = document.getElementById("about-btn");
+var imprintButton = document.getElementById("imprint-btn");
+var websiteGateButton = document.getElementById("website-gate-btn");
+
 function playVideo(video) {
   video.play();
 }
 
-document.addEventListener('click', function (event) {
-  if (!(event.target.matches('#about-btn') || event.target.closest('.video') || event.target.closest('.close-btn') || event.target.closest('#editing-btn') || event.target.closest('#ozzy-btn') || event.target.closest('#grading-btn') || event.target.matches('#imprint-btn') || event.target.matches('#website-gate-btn'))) return; // if we don't click any of these special elements, don't do anything, if we do, continue with doing different things depending on the element
-  if (event.target.matches('#about-btn')) {
+function handleMouseLeaveVideo(event) {
+  event.target.style.removeProperty('z-index'); // remove inline z-index and default back to initial value set with z-{} class
+  videoName.innerHTML = ''; // reset video display value to empty
+}
+
+function triggerModal(event) {
+  switch(event.target) {
+  case aboutButton:
     aboutPageContent.style.display = "flex";
     aboutPageContent.className = 'modal w-screen h-screen flex justify-center items-center text-white fixed top-0 z-70';
-  } else if (event.target.closest('.video')) {
+    break;
+  case imprintButton:
+    aboutPageContent.style.display = "none";
+    imprintContent.style.display = "flex";
+    imprintContent.className = 'modal z-70 fixed flex justify-center left-0 top-0 w-full h-full overflow-scroll';
+    break;
+  }
+}
+
+document.addEventListener('click', function (event) {
+  if (!(event.target.closest('.video') || event.target.closest('.close-btn'))) return; // if we don't click any of these special elements, don't do anything, if we do, continue with doing different things depending on the element
+  if (event.target.closest('.video')) {
     if (event.target.parentElement.firstElementChild.classList.contains('video-portrait')) {
       event.target.parentElement.firstElementChild.className = "block video-portrait"
       var newHTML = event.target.parentElement.firstElementChild.outerHTML; // save it's HTML
@@ -31,56 +58,6 @@ document.addEventListener('click', function (event) {
     modal.style.display = "none";
     aboutPageContent.style.display = "none";
     imprintContent.style.display = "none";
-  } else if (event.target.closest('#editing-btn')) {
-    // reset editing videos back to 100% brightness and remove inline z-index
-    let editingVideos = document.getElementsByClassName('video-editing');
-    for(var i=0; i < editingVideos.length; i++) {
-      editingVideos[i].style.filter = "brightness(100%)";
-      editingVideos[i].style.removeProperty('z-index');
-      let video = editingVideos[i].querySelector('video');
-      video.onmouseover = function() { playVideo(video) };
-    }
-    // dim grading videos
-    let gradingVideos = document.getElementsByClassName('video-grading');
-    for(var i=0; i < gradingVideos.length; i++) {
-      if (!gradingVideos[i].classList.contains('video-editing')) {
-        gradingVideos[i].style.filter = "brightness(15%)";
-        gradingVideos[i].style.zIndex = -1;
-        gradingVideos[i].querySelector('video').removeAttribute('onmouseover');
-      }
-    }
-  } else if (event.target.closest('#ozzy-btn')) {
-    let allVideos = document.getElementsByClassName('video');
-    for(var i=0; i < allVideos.length; i++) {
-      allVideos[i].style.filter = "brightness(100%)";
-      allVideos[i].style.removeProperty('z-index');
-      let video = allVideos[i].querySelector('video');
-      video.onmouseover = function() { playVideo(video) };
-    }
-  } else if (event.target.closest('#grading-btn')) {
-    // reset grading videos back to 100% brightness
-    let gradingVideos = document.getElementsByClassName('video-grading');
-    for(var i=0; i < gradingVideos.length; i++) {
-      gradingVideos[i].style.filter = "brightness(100%)";
-      gradingVideos[i].style.removeProperty('z-index');
-      let video = gradingVideos[i].querySelector('video');
-      video.onmouseover = function() { playVideo(video) };
-    }
-    // dim editing videos
-    let editingVideos = document.getElementsByClassName('video-editing');
-    for(var i=0; i < editingVideos.length; i++) {
-      if (!editingVideos[i].classList.contains('video-grading')) {
-        editingVideos[i].style.filter = "brightness(15%)";
-        editingVideos[i].style.zIndex = -1;
-        editingVideos[i].querySelector('video').removeAttribute('onmouseover');
-      }
-    }
-  } else if (event.target.matches('#imprint-btn')) {
-    aboutPageContent.style.display = "none";
-    imprintContent.style.display = "flex";
-    imprintContent.className = 'modal z-70 fixed flex justify-center left-0 top-0 w-full h-full overflow-scroll';
-  } else if (event.target.matches('#website-gate-btn')) {
-    websiteGate.style.display = "none";
   }
 }, false);
 
@@ -95,7 +72,55 @@ window.onclick = function(event) {
   }
 }
 
-function handleMouseLeaveVideo(event) {
-  event.target.style.removeProperty('z-index'); // remove inline z-index and default back to initial value set with z-{} class
-  videoName.innerHTML = ''; // reset video display value to empty
+editingButton.onclick = function(event) {
+  // reset editing videos back to 100% brightness and remove inline z-index
+  for(var i=0; i < editingVideos.length; i++) {
+    editingVideos[i].style.filter = "brightness(100%)";
+    editingVideos[i].style.removeProperty('z-index');
+    let video = editingVideos[i].querySelector('video');
+    video.onmouseover = function() { playVideo(video) };
+  }
+  // dim grading videos
+  for(var i=0; i < gradingVideos.length; i++) {
+    if (!gradingVideos[i].classList.contains('video-editing')) {
+      gradingVideos[i].style.filter = "brightness(15%)";
+      gradingVideos[i].style.zIndex = -1;
+      gradingVideos[i].querySelector('video').removeAttribute('onmouseover');
+    }
+  }
+}
+
+gradingButton.onclick = function(event) {
+  // reset grading videos back to 100% brightness
+  for(var i=0; i < gradingVideos.length; i++) {
+    gradingVideos[i].style.filter = "brightness(100%)";
+    gradingVideos[i].style.removeProperty('z-index');
+    let video = gradingVideos[i].querySelector('video');
+    video.onmouseover = function() { playVideo(video) };
+  }
+  // dim editing videos
+  for(var i=0; i < editingVideos.length; i++) {
+    if (!editingVideos[i].classList.contains('video-grading')) {
+      editingVideos[i].style.filter = "brightness(15%)";
+      editingVideos[i].style.zIndex = -1;
+      editingVideos[i].querySelector('video').removeAttribute('onmouseover');
+    }
+  }
+}
+
+ozzyButton.onclick = function(event) {
+  for(var i=0; i < allVideos.length; i++) {
+    allVideos[i].style.filter = "brightness(100%)";
+    allVideos[i].style.removeProperty('z-index');
+    let video = allVideos[i].querySelector('video');
+    video.onmouseover = function() { playVideo(video) };
+  }
+}
+
+aboutButton.onclick = function(event) { triggerModal(event) };
+
+imprintButton.onclick = function(event) { triggerModal(event) };
+
+websiteGateButton.onclick = function(event) {
+  websiteGate.style.display = "none";
 }
