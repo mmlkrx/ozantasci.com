df17a5f7f32452ac7eb1c0c30c4cc13f5bd36fbe
Matthias Michael Loker <m@theloker.com>
2019-10-30 22:39:09 +0100

    Fix missing css bug
    - [x] Debugging seems like deploy preview 8 didn’t compile all of the custom tailwind position utilities (maybe something with purgecss? Seems like text-neongreen for hover is there but not as a normal style WHICH OF COURSE makes sense because I’m only applying it with js duh) -> Same was true for why the work page didn’t work, I forgot to tell purgecss about it hence it purged all css used on the work page (former index)

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
     './public/index.html',
+    './public/work.html',
+    './public/js/index.js', // don't forget classes only applied with JS
+    './public/js/modal.js' // don't forget classes only applied with JS
   ],
 
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
