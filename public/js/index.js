2f1bdb1696fcaa34a7775dcf48de114a65b9653c
Matthias Michael Loker <m@theloker.com>
2019-10-31 17:12:02 +0100

    Add about and imprint content to index page

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,9 @@
 var video = document.getElementById("showreel");
 var soundToggleButton = document.getElementById("sound-toggle");
+var aboutButton = document.getElementById("about-btn");
+var aboutPageContent = document.getElementById("about-content");
+var imprintContent = document.getElementById("imprint-content");
+var imprintButton = document.getElementById("imprint-btn");
 
 function toggleMute() {
   if (video.muted){
@@ -15,4 +19,35 @@ function toggleMute() {
   }
 }
 
+function triggerModal(event) {
+  switch(event.target) {
+  case aboutButton:
+    aboutPageContent.style.display = "flex";
+    aboutPageContent.className = 'modal w-screen h-screen flex justify-center items-center text-white fixed top-0 z-70';
+    break;
+  case imprintButton:
+    aboutPageContent.style.display = "none";
+    imprintContent.style.display = "flex";
+    imprintContent.className = 'modal z-70 fixed flex justify-center left-0 top-0 w-full h-full overflow-scroll';
+    break;
+  }
+}
+
+document.addEventListener('click', function (event) {
+  if (!(event.target.closest('.close-btn'))) return; // if we don't click any of these special elements, don't do anything, if we do, continue with doing different things depending on the element
+  if (event.target.closest('.close-btn')) {
+    aboutPageContent.style.display = "none";
+    imprintContent.style.display = "none";
+  }
+}, false);
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == aboutPageContent) {
+    aboutPageContent.style.display = "none";
+  }
+}
+
 soundToggleButton.onclick = function(event) { toggleMute(event) };
+aboutButton.onclick = function(event) { triggerModal(event) };
+imprintButton.onclick = function(event) { triggerModal(event) };
