e0185f5db3240279353a6e6a69b32ae633c02ab8
Matthias Michael Loker <m@theloker.com>
2019-12-13 15:44:27 +0100

    Add netlify _redirects file

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(config) {
   config.addPassthroughCopy("site/img"); // pass images folder right through to output folder
   config.addPassthroughCopy("site/videos"); // pass videos folder right through to output folder
   config.addPassthroughCopy("site/js"); // pass js folder right through to output folder
+  config.addPassthroughCopy("site/_redirects"); // _redirects file must be in deployed folder for Netlify
 
   return {
     dir: {
